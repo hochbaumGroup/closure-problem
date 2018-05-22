3eda636f6fdaafc5fcd2d3c8b13ca544a3b52795
Quico Spaen <quico@spaen.nl>
2018-05-21 20:58:18 -0700

    Add code for solving parametrized closure problems

--- src/closure/closure.py
+++ src/closure/closure.py
@@ -3,7 +3,12 @@ from pseudoflow import hpf
 
 
 class Closure(object):
-    """Class for closure problems on directed graphs"""
+    """Class for closure problems on directed graphs
+
+    NOTE: Only works correctly when source adjacent arcs have increasing
+    parameter weights and sink adjacent arcs have negative parameter weights.
+
+    """
 
     def __init__(self, graph, node_weight='weight', arc_weight=None):
         if arc_weight is None:
@@ -13,23 +18,54 @@ class Closure(object):
 
         graph.add_nodes_from(['s', 't'])
 
-        for node in graph.nodes:
-            if node not in ('s', 't'):
-                weight = graph.nodes[node][node_weight]
-                if weight > 0:
+        if isinstance(node_weight, str):
+            constant = node_weight
+            multiplier = None
+        else:
+            constant, multiplier = node_weight
+
+        if multiplier:
+            for u, v in graph.edges:
+                graph[u][v]['multiplier'] = 0
+
+            for node in graph.nodes:
+                if node not in ('s', 't'):
+                    weight = graph.nodes[node][constant]
+                    multiplier_weight = graph.nodes[node][multiplier]
                     graph.add_edge(node, 't',
-                                   **{arc_weight: weight})
-                elif weight < 0:
+                                   **{arc_weight: weight,
+                                      'multiplier': multiplier_weight})
                     graph.add_edge('s', node,
-                                   **{arc_weight: -weight})
+                                   **{arc_weight: -weight,
+                                      'multiplier': -multiplier_weight})
+        else:
+            for node in graph.nodes:
+                if node not in ('s', 't'):
+                    weight = graph.nodes[node][constant]
+                    if weight > 0:
+                        graph.add_edge(node, 't',
+                                       **{arc_weight: weight})
+                    elif weight < 0:
+                        graph.add_edge('s', node,
+                                       **{arc_weight: -weight})
 
         self._G = graph
-        self._node_weight = node_weight
+        self._constant = constant
         self._arc_weight = arc_weight
 
     def solve(self):
         _, cuts, _ = hpf(self._G, 's', 't', self._arc_weight)
 
-        print(cuts)
-
         return {x for x in self._G if cuts[x][0] == 1 and x not in ('s', 't')}
+
+    def solve_parametric(self, parameter_range):
+        breakpoints, cuts, _ = hpf(
+            self._G, 's', 't', self._arc_weight, mult_cap='multiplier',
+            lambdaRange=parameter_range, roundNegativeCapacity=True)
+
+        cuts = [
+            {x for x in self._G if cuts[x][i] == 1 and x not in ('s', 't')}
+            for i in range(len(breakpoints))
+        ]
+
+        return cuts, breakpoints
