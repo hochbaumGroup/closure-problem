2e6b9f83e438869f9a69334eedc0b69ee2b3737a
Quico Spaen <quico@spaen.nl>
2018-05-21 18:16:45 -0700

    Add test for parameteric closure problem

--- tests/test_closure.py
+++ tests/test_closure.py
@@ -6,7 +6,7 @@ import networkx as nx
 def G():
     G = nx.DiGraph()
 
-    G.add_node(0, weight=3)
+    G.add_node(0, weight=3, multiplier=-2)
     G.add_node(1, weight=-6)
     G.add_node(2, weight=4)
 
@@ -31,18 +31,31 @@ def S_Excess(G):
     return Closure(G, arc_weight='weight')
 
 
+@pytest.fixture
+def C_parametric(G):
+    from closure.closure import Closure
+
+    return Closure(G, node_weight=('weight', 'multiplier'))
+
+
 def test_min_closure(C):
     set = C.solve()
     print(set)
 
-    assert 1 in set
-    assert 2 in set
-    assert len(set) == 2
+    assert set == {1, 2}
 
 
 def test_min_s_excess(S_Excess):
     set = S_Excess.solve()
     print(set)
 
-    assert 1 in set
-    assert len(set) == 1
+    assert set == {1, }
+
+
+def test_min_closure_parametric(C_parametric):
+    sets, breakpoints = C_parametric.solve_parametric([0, 5])
+    print(sets, breakpoints)
+
+    assert breakpoints == [0, 1.5]
+    assert sets[0] == {1, 2}
+    assert sets[1] == {0, 1, 2}
