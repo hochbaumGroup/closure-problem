e3c7c849d170ca501520ee0d6d2901c604ebffe1
Quico Spaen <quico@spaen.nl>
2019-07-01 14:02:47 -0700

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="closure-problem",
-    version="2019.6.1",
+    version="2019.6.2",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
