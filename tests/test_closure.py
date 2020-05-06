a3a779756c38fe5aaacdc01c68ffa11746f4a76c
Quico Spaen <quico@spaen.nl>
2020-05-05 20:50:58 -0700

    Fix with unsupported multipliers + simplify test file

--- tests/test_closure.py
+++ tests/test_closure.py
@@ -1,14 +1,16 @@
 import pytest
 import networkx as nx
 
+from closure.closure import Closure
+
 
 @pytest.fixture
 def G():
     G = nx.DiGraph()
 
-    G.add_node(0, weight=3, multiplier=0)
-    G.add_node(1, weight=-6, multiplier=0)
-    G.add_node(2, weight=4, multiplier=2)
+    G.add_node(0, weight=3)
+    G.add_node(1, weight=-6)
+    G.add_node(2, weight=4)
 
     G.add_edge(0, 1, weight=1)
     G.add_edge(0, 2, weight=1)
@@ -16,43 +18,27 @@ def G():
 
     return G
 
-
-@pytest.fixture
-def C(G):
-    from closure.closure import Closure
-
-    return Closure(G)
+def test_min_closure(G):
+    assert Closure(G).solve() == {1, 2}
 
 
-@pytest.fixture
-def S_Excess(G):
-    from closure.closure import Closure
-
-    return Closure(G, arc_weight='weight')
+def test_min_s_excess(G):
+    assert Closure(G, arc_weight='weight').solve() == {1, }
 
 
-@pytest.fixture
-def C_parametric(G):
-    from closure.closure import Closure
+def test_min_closure_parametric():
+    G = nx.DiGraph()
 
-    return Closure(G, node_weight=('weight', 'multiplier'))
+    G.add_node(0, weight=3, multiplier=0)
+    G.add_node(1, weight=-6, multiplier=0)
+    G.add_node(2, weight=8, multiplier=-1)
 
+    G.add_edge(0, 1, weight=1)
+    G.add_edge(0, 2, weight=1)
+    G.add_edge(1, 2, weight=1)
 
-def test_min_closure(C):
-    set = C.solve()
+    sets, breakpoints = Closure(G, node_weight=('weight', 'multiplier')).solve_parametric(0, 3)
 
-    assert set == {1, 2}
-
-
-def test_min_s_excess(S_Excess):
-    set = S_Excess.solve()
-
-    assert set == {1, }
-
-
-def test_min_closure_parametric(C_parametric):
-    sets, breakpoints = C_parametric.solve_parametric(0, 5)
-
-    assert breakpoints == [1, 5.0]
-    assert sets[0] == {1, 2}
-    assert sets[1] == set()
+    assert breakpoints == [2, 3.0]
+    assert sets[0] == set()
+    assert sets[1] == {1, 2}
